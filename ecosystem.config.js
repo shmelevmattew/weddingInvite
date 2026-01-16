f7cc03320d82859c54fb2336020e129baddebb24
@SHMELEV183_EXT <shmelev.mv@mmk.ru>
2026-01-16 11:36:26 +0500

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [{
+    name: 'wedding',
+    script: 'server.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    env: {
+      PORT: 3000,
+      TELEGRAM_BOT_TOKEN: '8592554071:AAHr9eA_RBeqKuctNudbvYn3_VUpGK7m7BY',
+      TELEGRAM_CHAT_ID: '654643546'
+    }
+  }]
+};
+
